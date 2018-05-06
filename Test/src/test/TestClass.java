b5f42e9d7f0debe5ee5e9271b0f265900eb232f3
JitenderPal <palljiten@gmail.com>
2018-05-07 00:53:49 +0530

    fourth

---
+++ Test/src/test/TestClass.java
@@ -0,0 +1,19 @@
+package test;
+
+public class TestClass 
+{
+   String var;
+   public String getVar() {
+	return var;
+}
+public void setVar(String var) {
+	this.var = var;
+}
+public String getP() {
+	return p;
+}
+public void setP(String p) {
+	this.p = p;
+}
+String p;
+}
